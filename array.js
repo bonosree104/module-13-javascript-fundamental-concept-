345c2e20c180ff59df409ea4f500d53126ad53ee
bonosree104 <bonosreeela104@gmail.com>
2021-06-29 16:14:16 +0600

    my website

---
+++ array.js
@@ -0,0 +1,11 @@
+var friendAge = [15, 17, 18, 20];
+
+ var rupaliAge = friendAge[2];
+
+  friendAge[1] = 50;
+
+  var position = friendAge.indexOf(181);
+  friendAge.push(44);
+  friendAge. push(33);
+  friendAge.pop();
+console.log(friendAge.length);
